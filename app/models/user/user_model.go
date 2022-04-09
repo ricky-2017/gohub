e54c25b4757388ad2947708e9fd6c9d565d4867e
rubio <793212775@qq.com>
2022-04-09 16:35:07 +0800

    feat.UserModel

---
+++ app/models/user/user_model.go
@@ -0,0 +1,16 @@
+package user
+
+import (
+	"gohub/app/models"
+)
+
+type User struct {
+	models.BaseModel
+
+	Name     string `json:"name,omitempty"`
+	Email    string `json:"-"`
+	Phone    string `json:"-"`
+	Password string `json:"-"`
+
+	models.CommonTimestampsField
+}
---
+++ app/models/user/user_util.go
@@ -0,0 +1,17 @@
+package user
+
+import "gohub/pkg/database"
+
+// 判断邮箱已存在
+func IsEmailExist(email string) bool {
+	var count int64
+	database.DB.Model(User{}).Where("email = ?", email).Count(&count)
+	return count > 0
+}
+
+// 判断手机号是否已存在
+func IsPhoneExist(phone string) bool {
+	var count int64
+	database.DB.Model(User{}).Where("phone = ?", phone).Count(&count)
+	return count > 0
+}
