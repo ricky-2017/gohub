5672ae633bf43179822e2d308768d49dbc9137f6
rubio <793212775@qq.com>
2022-04-12 18:05:15 +0800

    logger

--- bootstrap/logger.go
+++ bootstrap/logger.go
@@ -1,8 +1,11 @@
 package bootstrap
 
 import (
+	"encoding/json"
 	"gohub/pkg/config"
 	"gohub/pkg/logger"
+
+	"go.uber.org/zap"
 )
 
 // SetupLogger 初始化 Logger
@@ -17,4 +20,22 @@ func SetupLogger() {
 		config.GetString("log.type"),
 		config.GetString("log.level"),
 	)
+}
+
+func jsonString(value interface{}) string {
+	b, err := json.Marshal(value)
+	if err != nil {
+		Logger.Error("Logger", zap.String("JSON marshal error", err.Error()))
+	}
+	return string(b)
+}
+
+func Dump(value interface{}, msg ...string) {
+	valueString := jsonString(value)
+	// 判断第二个参数是否传参 msg
+	if len(msg) > 0 {
+		Logger.Warn("Dump", zap.String(msg[0], valueString))
+	} else {
+		Logger.Warn("Dump", zap.String("data", valueString))
+	}
 }
